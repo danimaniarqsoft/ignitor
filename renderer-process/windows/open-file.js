cfe2ceeaa9b3388f0c9b8db196206f084695da99
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-10-05 00:17:04 -0500

    adding new module

---
+++ renderer-process/windows/open-file.js
@@ -0,0 +1,50 @@
+const {ipcRenderer} = require('electron')
+const fs = require('fs');
+const walk = require('walk');
+const path = require('path')
+
+const selectDirBtn = document.getElementById('select-directory')
+
+const notification = {
+  title: 'Kukulkan :: Full Scan Finished',
+  body: 'Kukulkan have finish your scan, check out the results',
+  icon: path.join(__dirname, '../../../assets/img/programming.png')
+}
+
+var walker;
+ 
+  options = {
+    followLinks: false
+  , filters: ["Temp", "_Temp"]
+  };
+
+selectDirBtn.addEventListener('click', (event) => {
+  ipcRenderer.send('open-file-dialog')
+})
+
+ipcRenderer.on('selected-directory', (event, path) => {
+  document.getElementById('selected-file').innerHTML = `Scan: ${path}`
+  walker = walk.walk(`${path}`, options);
+  var files = [];
+
+  walker.on("directories", function (root, dirStatsArray, next) {
+    next();
+  });
+ 
+  walker.on("file", function (root, fileStats, next) {
+    files.push(fileStats.name);
+    fs.readFile(fileStats.name, function () {
+      next();
+    });
+  });
+ 
+  walker.on("errors", function (root, nodeStatsArray, next) {
+    next();
+  });
+ 
+  walker.on("end", function () {
+    const myNotification = new window.Notification(notification.title, notification)
+    document.getElementById('selected-file').innerHTML = `Scan: ${files}`
+  });
+
+})
