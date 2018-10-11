2b85dab9233b99816fab8caf540f02b515623720
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-10-10 19:07:34 -0500

    adding meta files

--- renderer-process/windows/scan-files.js
+++ renderer-process/windows/scan-files.js
@@ -4,10 +4,16 @@ var fs = require('fs'); // you can select any filesystem as long as it implement
  
 DirectoryStructureJSON.getStructure(fs, basepath, function (err, structure, total) {
     if (err) console.log(err);
-    console.log('there are a total of: ', total.folders, ' folders and ', total.files, ' files');
-    console.log('the structure looks like: ', JSON.stringify(structure, null, 4));
-    fs.writeFile('/home/daniel/Dropbox/estructura-scm/estructura/dir-spec.json', JSON.stringify(structure, null, 4), (err) => {
+    fs.writeFile('/home/daniel/Dropbox/estructura-scm/estructura/.meta-info.json', JSON.stringify(structure, null, 4), (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
       });
 });
+
+DirectoryStructureJSON.traverseStructure(structure, basepath,
+    function (folder, path) {
+        console.log('folder found: ', folder.name, 'at path: ', path);
+    },
+    function (file, path) {
+        console.log('file found: ', file.name, 'at path: ', path);
+    });
