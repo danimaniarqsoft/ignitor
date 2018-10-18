58414f48923daf844d81339bc38a63fcd780d19d
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-10-17 22:21:01 -0500

    addin new dependencies

--- test/diff-folder-test.js
+++ test/diff-folder-test.js
@@ -1,17 +1,16 @@
 'use strict'
-// const fs = require('fs')
-const dirTree = require('directory-tree');
-const diff = require('../util/diff-folder').diff
-const printFolder = require('../util/print-folder').printFolder
-const path = require('path')
 
+const dirTree = require('directory-tree');
+const scanFiles = require('scan-files')
+const path = require('path')
+const fs = require('fs')
 const basepath = 'resources'
-var leftStrPath = path.join(__dirname, basepath, 'leftStr')
+
+var leftStrPath = path.join(__dirname, basepath, 'repository-template.json')
 var rightStrPath = path.join(__dirname, basepath, 'rightStr')
-var leftTree = dirTree(leftStrPath);
+var leftTree = JSON.parse(fs.readFileSync(leftStrPath, 'utf8'));
 var rightTree = dirTree(rightStrPath)
-var difference = diff(leftTree, rightTree)
-//console.log(difference.result)
-//console.log(JSON.stringify(leftTree, null, 4))
-printFolder(difference.result)
-//console.log(JSON.stringify(difference.result, null, 4))
+
+var difference = scanFiles.diff(leftTree, rightTree)
+
+console.log(difference.result)
